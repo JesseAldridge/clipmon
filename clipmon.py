31fe5dda714af2b38714a0819e099e944533215d
Jesse Aldridge <JesseAldridge@gmail.com>
2015-11-06 10:30:59 -0800

    switch to xerox

--- clipmon.py
+++ clipmon.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import time, sys, os, subprocess, re
 
-import clipboard
+import xerox
 
 def clip_str_to_path_line(clip_str):
   clip_str = clip_str.replace('http://localhost:5000', os.path.expanduser('~/Dropbox/CardBrew'))
@@ -18,7 +18,7 @@ if __name__ == '__main__':
   clip_str = None
   while True:
     prev_value = clip_str
-    clip_str = clipboard.paste()
+    clip_str = xerox.paste()
     if prev_value is not None and clip_str != prev_value:
       print 'new value:', clip_str
       path_line = clip_str_to_path_line(clip_str)
