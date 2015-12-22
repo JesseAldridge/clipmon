c63fbdbd728b6522fe6798e59930a130cee4fddc
Jesse Aldridge <jesse@gigwalk.com>
2015-12-22 15:01:25 -0800

    add configurable current project

---
+++ conf.py
@@ -0,0 +1,9 @@
+
+# ----- Specify your configuration here. -----
+
+curr_proj_dir = '/Users/jessealdridge/gigwalk/apps/gigwalk_apps_platform_api'
+
+# ----- End of your configuration. ------
+
+if not curr_proj_dir.endswith('/'):
+  curr_proj_dir += '/'
