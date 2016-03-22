e38873b6ad903699640fd426cdc5f77f5ba3f24f
Jesse Aldridge <JesseAldridge@gmail.com>
2016-03-22 13:38:30 -0700

    refactor to use loop

--- clipmon.py
+++ clipmon.py
@@ -10,17 +10,30 @@ import conf
 def clip_str_to_path_line(clip_str):
     if clip_str.count('\n') > 1:
         return
-    # test full path
-    result_line = test_full_match(clip_str)
-    if result_line:
-        return result_line
-    else:
-        for proj_dir in conf.curr_proj_dirs:
-            replaced_str = clip_str.replace('http://localhost:5000/', proj_dir)
-            result_line = test_full_match(replaced_str)
-            if result_line:
-                return result_line
 
+    for f in test_full_match, test_repl_localhost, test_parital_path:
+        result_line = f(clip_str)
+        if result_line:
+            return result_line
+
+def test_full_match(clip_str):
+    match = re.search(
+        #             file extension
+        # path            |
+        #  |              |
+        r'(/[^@^:^\\^\(]+\.[a-z]{2,3})[^:]{0,9}(line.*?|:|\()([0-9]+)', clip_str)
+    if match and path_exists(match.group(1)):
+        return ':'.join([match.group(1), match.group(3)])
+
+def test_repl_localhost(clip_str):
+    for proj_dir in conf.curr_proj_dirs:
+        replaced_str = clip_str.replace('http://localhost:5000/', proj_dir)
+        result_line = test_full_match(replaced_str)
+        if result_line:
+            return result_line
+
+
+def test_parital_path(clip_str):
     # test partial path
     match = re.search(
         r'([a-zA-Z_/\-\.0-9]+/[a-zA-Z_0-9\-]+\.[a-z]{2,3})[^:]{0,9}(line.*?|:)([0-9]+)', clip_str)
@@ -33,14 +46,6 @@ def clip_str_to_path_line(clip_str):
             if path_exists(full_path):
                 return ':'.join([full_path, match.group(3)])
 
-def test_full_match(clip_str):
-    match = re.search(
-        #             file extension
-        # path            |
-        #  |              |
-        r'(/[^@^:^\\^\(]+\.[a-z]{2,3})[^:]{0,9}(line.*?|:|\()([0-9]+)', clip_str)
-    if match and path_exists(match.group(1)):
-        return ':'.join([match.group(1), match.group(3)])
 
 def path_exists(path):
     return path in conf.test_whitelist if conf.test_whitelist else os.path.exists(path)
